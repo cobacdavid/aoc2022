8e608504efe7388ea7da01460849cc7614864365
cobacdavid <david.cobac@gmx.fr>
2022-12-06 11:39:17 +0100

    day06 - pb1 pb2

---
+++ day06/pb1_poo.py
@@ -0,0 +1,30 @@
+class Chaine:
+    def __init__(self, v):
+        self._chaine = v
+
+    @property
+    def chaine(self):
+        return self._chaine
+
+    @chaine.setter
+    def chaine(self, c):
+        self ._chaine = c
+
+    def _tous_differents(self, indice, lng):
+        reponse = False
+        if indice + lng - 1< len(self._chaine):
+            reponse = len(set(self._chaine[indice:indice+lng])) == lng
+        return reponse
+
+    def pindice_tous_differents(self, lng):
+        indice = 0
+        while indice < len(self._chaine) and not self._tous_differents(indice, lng):
+            indice += 1
+        return indice + lng
+
+
+fichier = "jeu2.dat"
+with open(fichier) as fh:
+    for ligne in fh:
+        ligne = ligne.strip()
+        print(Chaine(ligne).pindice_tous_differents(4))
